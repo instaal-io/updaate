be14f43aea372aebe7177b93313107ef23e5b45d
ahmmedrejowan <ahmmedrejowan@gmail.com>
2022-02-21 22:53:42 +0600

    Function Changes

--- app/src/main/java/io/instaal/updatechecker/MainActivity.java
+++ app/src/main/java/io/instaal/updatechecker/MainActivity.java
@@ -1,11 +1,10 @@
 package io.instaal.updatechecker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import io.instaal.miniupdatechecker.MiniUpdateChecker;
 
@@ -17,29 +16,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = findViewById(R.id.image_button);
-    //    imageView.setOnClickListener(view -> {
-            new MiniUpdateChecker(this)
-                    .setTheme(MiniUpdateChecker.DEFAULT_THEME)
-                    .setTitle("App Updater")
-                    .setAppIcon(R.drawable.ic_baseline_10mp_24)
-                    .setCloseIcon(R.drawable.ic_android_black_24dp)
-                    .setDescription("New Update For")
-                    .setAppName("MY APP")
-                    .setPositiveLabel("Do It")
-                    .setNegativeLabel("Never")
-                    .setBackgroundColor(Color.parseColor("#eeeeee"))
-                    .setPositiveButtonColor(Color.parseColor("#FFA726"))
-                    .setNegativeButtonColor(Color.parseColor("#FF7043"))
-                    .setPrimaryTextColor(R.color.colorAccent)
-                    .setSecondaryTextColor(Color.MAGENTA)
-                    .setPositiveTextColor(R.color.black)
-                    .setNegativeTextColor(R.color.white)
-                    .shouldCheckAfterLaunch(1)
-                    .check();
+        CardView cardView = findViewById(R.id.checkButton);
+        cardView.setOnClickListener(view -> checkForUpdate());
 
-     //   });
-
+        checkForUpdate();
 
     }
+
+    private void checkForUpdate() {
+        new MiniUpdateChecker(this)
+                .setTheme(MiniUpdateChecker.DEFAULT_THEME)
+                .setTitle("App Updater")
+                .setAppIcon(R.drawable.ic_baseline_10mp_24)
+                .setCloseIcon(R.drawable.ic_android_black_24dp)
+                .setDescription("New Update For")
+                .setAppName("MY APP")
+                .setPositiveLabel("Do It")
+                .setNegativeLabel("Never")
+                .setBackgroundColor(Color.parseColor("#eeeeee"))
+                .setPositiveButtonColor(Color.parseColor("#FFA726"))
+                .setNegativeButtonColor(Color.parseColor("#FF7043"))
+                .setPrimaryTextColor(R.color.colorAccent)
+                .setSecondaryTextColor(Color.MAGENTA)
+                .setPositiveTextColor(R.color.black)
+                .setNegativeTextColor(R.color.white)
+                .shouldCheckAfterLaunch(1)
+                .check();
+    }
+
+
 }
