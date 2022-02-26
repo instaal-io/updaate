c447e29742793b848e28d7f85fe3fb3a31dcbcbb
ahmmedrejowan <ahmmedrejowan@gmail.com>
2022-02-26 09:50:47 +0600

    Code Cleanup

--- app/src/main/java/io/instaal/updatechecker/MainActivity.java
+++ app/src/main/java/io/instaal/updatechecker/MainActivity.java
@@ -1,6 +1,5 @@
 package io.instaal.updatechecker;
 
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,55 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CardView cardView = findViewById(R.id.checkButton);
-        cardView.setOnClickListener(view ->
-                new updaate(this)
-                        .setTheme(updaate.DEFAULT_THEME)
-                        .setPrimaryColor(R.color.colorAccent)
-                        .setSecondaryColor(R.color.colorPrimary)
-                        .setPositiveTextColor(R.color.white)
-                        .setTitle("A New Update Available")
-                        .setPositiveLabel("Update")
-                        .setNegativeLabel("Later")
-                        .setNegativeButtonColor(R.color.white)
-                        .setBackgroundColor(Color.WHITE)
-                        .shouldCheckAfterLaunch(1)
-                        .check());
+        cardView.setOnClickListener(view -> new updaate(this).setTheme(updaate.DEFAULT_THEME).check());
 
 
         CardView cardView2 = findViewById(R.id.checkButton2);
-        cardView2.setOnClickListener(view ->
-                new updaate(this)
-                        .setTheme(updaate.SMART_THEME)
-                        .setPrimaryColor(R.color.colorAccent)
-                        .setSecondaryColor(R.color.colorPrimary)
-                        .setPositiveTextColor(R.color.white)
-                        .setIconColor(R.color.black)
-                        .setTitle("A New Update Available")
-                        .setAppIcon(R.drawable.update_icon)
-                        .setPositiveLabel("Update")
-                        .setNegativeLabel("Later")
-                        .setNegativeButtonColor(R.color.white)
-                        .setBackgroundColor(Color.WHITE)
-                        .shouldCheckAfterLaunch(1)
-                        .check());
+        cardView2.setOnClickListener(view -> new updaate(this).setTheme(updaate.SMART_THEME).check());
 
         CardView cardView3 = findViewById(R.id.checkButton3);
-        cardView3.setOnClickListener(view ->
-                new updaate(this)
-                        .setTheme(updaate.FANCY_THEME)
-                        .setPrimaryColor(R.color.colorAccent)
-                        .setSecondaryColor(R.color.colorPrimary)
-                        .setPositiveTextColor(R.color.white)
-                        .setTitle("A New Update Available")
-                        .setPositiveLabel("Update")
-                        .setNegativeLabel("Later")
-                        .setBackgroundColor(Color.WHITE)
-                        .shouldCheckAfterLaunch(1)
-                        .check());
+        cardView3.setOnClickListener(view -> new updaate(this).setTheme(updaate.FANCY_THEME).check());
 
 
     }
-
 
 
 }
